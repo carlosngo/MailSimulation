b916ae50f8cca1fd2868023e565087e2736b4e83
Carlos <carlos_ngo@dlsu.edu.ph>
2018-07-12 21:56:40 +0800

    Update Mail.java

--- Model/Mail.java
+++ Model/Mail.java
@@ -1,5 +1,22 @@
 package Model;
 
 public class Mail {
+    private String recipient;
+    private Location destination;
+    private DateTime dateTime;
+
+    public Mail(String recipient, Location destination, DateTime dateTime) {
+        this.recipient = recipient;
+        this.destination = destination;
+        this.dateTime = dateTime;
+    }
     
+    public Location getDestination() {
+        return destination;
+    }
+    
+    @Override
+    public boolean equals(Object obj) {
+        
+    }
 }

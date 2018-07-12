2c15ed11d2f067d45a10bbba37c6b9779fc6b5d9
Carlos <carlos_ngo@dlsu.edu.ph>
2018-07-12 22:14:14 +0800

    Update Mailman.java

--- Model/Mailman.java
+++ Model/Mailman.java
@@ -1,5 +1,62 @@
 package Model;
+import java.util.*;
+import java.io.*;
 
 public class Mailman {
+    String name;
+    private ArrayList<Map> maps;
+    private int currentMapIndex;
+    private ArrayList<Mail> bag;
+    private ArrayList<Mail> sorted;
     
+    /**
+     * NOTE: read csv files upon instantiation.
+     */
+    public Mailman(String name) {
+        
+    }
+    
+    public void addMap(Map m) {
+        
+    }
+    
+    /**
+     * reads the CSV file. add a map for every region.
+     */
+    public void readMaps(File csv) {
+        
+    }
+    
+    /**
+     * fetches the new batch of mails in the current region's
+     * post office. add each mail to bag.
+     */
+    public void fetchMail() {
+        
+    }
+    
+    /**
+     * puts all mail with destinations within
+     * the current region in the ArrayList variable 
+     * sorted, and then sorts it (any sort u prefer).
+     */
+    public void sortMail() {
+        
+    }
+    
+    /**
+     * removes all mail from the current region.
+     * to be called after displaying the required output.
+     */
+    public void deliverMail() {
+        
+    }
+    
+    public void displaySortedMails() {
+        
+    }
+    
+    public void displayRoute() {
+        
+    }
 }

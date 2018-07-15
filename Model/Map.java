a4dd4b036fbad12d1594d397ae4182b3af82f4aa
samuraimac <39005419+samuraimac@users.noreply.github.com>
2018-07-15 23:00:57 +0800

    Update Map.java

--- Model/Map.java
+++ Model/Map.java
@@ -1,5 +1,20 @@
 package Model;
 
-public class Map {
-    
+public class Map{
+
+	private String region;
+	private ArrayList<Location> locations;
+
+	public Map(String region){
+		this.region = region;
+		locations = new ArrayList<Location>();
+	}
+
+	public void addLocation(Location l){
+		locations.add(l);
+	}
+
+	public PostOffice getPostOffice(){
+
+	}
 }

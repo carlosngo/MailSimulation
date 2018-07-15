697eaf59476b69596bf567987cbd13bc0fb2dff3
samuraimac <39005419+samuraimac@users.noreply.github.com>
2018-07-15 23:03:32 +0800

    Update PostOffice.java

--- Model/PostOffice.java
+++ Model/PostOffice.java
@@ -1,5 +1,19 @@
 package Model;
 
-public class PostOffice extends Location {
-    
+public class PostOffice extends Location{
+
+	private String name;
+	private ArrayList<Mail> mails;
+
+	public PostOffice(String name){
+		this.name = name;
+	}
+
+	public ArrayList<Mail> pushMail(){
+
+	}
+
+	public ArrayList<Mail> generateMail(){
+
+	}
 }

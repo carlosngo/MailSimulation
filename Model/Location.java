7942e6b902c0716d8b89939b7d9d1d7eb3441174
Carlos <carlos_ngo@dlsu.edu.ph>
2018-07-17 17:41:39 +0800

    Update Location.java

--- Model/Location.java
+++ Model/Location.java
@@ -1,34 +1,42 @@
 package Model;
 
+import java.util.*;
+import java.io.*;
+
 public class Location {
 
     String name;
     String region;
     ArrayList<Edge> connections;
 
-    public Location(String name, String region){
-    	this.name = name;
-    	this.region = region;
+    public Location(String name, String region) {
+        this.name = name;
+        this.region = region;
+        connections = new ArrayList<>();
     }
 
-    public int getDistance(Location l){
-    	//calculate for the distance then return it
+    public int getShortestPath(Location l) {
+        //calculate for the distance then return it
     }
 
-    public String getRegion(){
-    	return region;
+    public String getRegion() {
+        return region;
     }
 
-    public String getName(){
-    	return name;
+    public String getName() {
+        return name;
     }
 
-    public void addConnection(Edge e){
-    	connections.add(e);
+    public ArrayList<Edge> getConnections() {
+        return connections;
+    }
+    
+    public void addConnection(Edge e) {
+        connections.add(e);
     }
 
-    public boolean equals(Object obj){
-    	Location l = (Location) obj;
-    	return name.equals(l.name) && region.equals(l.region);
+    public boolean equals(Object obj) {
+        Location l = (Location) obj;
+        return name.equals(l.name) && region.equals(l.region);
     }
 }

0ae31e876b1942697769c5047e02698e60dbcd4e
samuraimac <39005419+samuraimac@users.noreply.github.com>
2018-07-19 21:43:35 +0800

    Update MailsList.java

--- View/MailsList.java
+++ View/MailsList.java
@@ -6,23 +6,43 @@
 package View;
 
 import Model.*;
-import java.util.*;
 import javax.swing.*;
+import javax.swing.event.*;
 import java.awt.*;
 import java.awt.event.*;
-/**
- *
- * @author User
- */
-public class MailsList extends JFrame implements ActionListener {
-    ArrayList<Mail> list;
+
+public class MailsList extends JFrame implements ActionListener{
     
-    public MailsList(ArrayList<Mail> list) {
-        this.list = list;
+    Mailman m;
+    JButton done = new JButton();
+    
+    public MailsList(){
+        m = new Mailman("messenger boy");
+        initMailsList();
     }
     
-    public void actionPerformed(ActionEvent e) {
+    public void initMailsList(){
+        JPanel p = new JPanel();
+        p.setLayout(new BoxLayout(p,BoxLayout.Y_AXIS));
         
+        JLabel mail = new JLabel();
+        for(int i=0;i<m.getBag().size();i++){
+            mail.setText(m.getBag().get(i).toString());
+            p.add(mail);
+            p.add(Box.createRigidArea(new Dimension(0,10)));
+        }
+       
+       done = new JButton("Done");
+       done.setAlignmentX(Component.CENTER_ALIGNMENT);
+       done.setFont(new Font("Abril Fatface", Font.PLAIN, 20));
+       done.addActionListener (this);
+       p.add(done);
+       p.add(Box.createRigidArea(new Dimension(0,10))); 
     }
     
+    public void actionPerformed (ActionEvent e){
+        if(e.getActionCommand ().equals ("Done")){
+            dispose();
+        }
+    }
 }
